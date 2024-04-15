811526605c84775ccb6ae580895a41ce36b5a242
Justin Higgins <justinhiggy@gmail.com>
2024-04-15 12:46:08 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import useSound from 'use-sound';
+import sound_A from './a.mp3';
+
+const PlayA = () => {
+  const [playA] = useSound(sound_A);
+  return <button onClick={playA}>vowl "A"!</button>;
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PlayA />
     </div>
   );
 }
